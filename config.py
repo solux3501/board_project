58d573f7d8c4ecd544a88a11e5ea7c1ec0e6f17b
solux3501 <70375441+solux3501@users.noreply.github.com>
2025-09-27 23:57:27 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))  # 현재 경로 -> 절대 경로로 변경한 후 저장
 
+BASE_DIR = os.path.dirname(__file__)
 
-class Config :
- SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-me'  # key : 비밀키가 있으면 전자, 아니면 후자
- SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-  'sqlite:///' + os.path.join(basedir, 'app.db')  # DATABASE 경로 : 있으면 전자, 없으면 후자
- SQLALCHEMY_TRACK_MODIFICATION = False  # 그냥 필요 없는 기능이라 생각
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = "dev"
